8f4848b615646bef9afcc87417d6b6a9d301c4b3
Jackkazan <antoniorub02@gmail.com>
2023-11-24 16:16:43 +0100

    Cambiata la cartella Tileset per malcorrispondenza nei percorsi

---
+++ src/main/resources/Tileset/TilesetsZonaIniziale/Tileset-Animated Terrains-8 frames-.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tileset-Animated Terrains-8 frames-" tilewidth="16" tileheight="16" tilecount="1044" columns="58">
+ <image source="../Tileset/EPIC RPG World Pack - [FREE Demo]Ancient Ruins - Copia/Tilesets/Tileset-Animated Terrains-8 frames-.png" width="928" height="288"/>
+</tileset>
