8530ee6e9ab9500ae3d135f101d3d0fa7fbc3e9d
SozLui <luigi.la.barbera1999@gmail.com>
2023-12-07 19:09:45 +0100

    Pause system

--- src/main/java/controller/KeyHandler.java
+++ src/main/java/controller/KeyHandler.java
@@ -1,18 +1,33 @@
 package controller;
 
+import view.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
 
+    private GamePanel gamePanel;
+
     public boolean upPressed, downPressed, leftPressed, rightPressed, interactPressed;
 
     private boolean showDebugText = false;
+    private boolean pausePressed = false;
+    private boolean pauseToggle = true;
 
     @Override
     public void keyTyped(KeyEvent e) {
 
     }
+
+    public KeyHandler(GamePanel gamePanel) {  // Aggiunto
+        this.gamePanel = gamePanel;  // Aggiunto
+    }
+
+    public void setGamePanel(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
+
 
     @Override
     public void keyPressed(KeyEvent e) {
@@ -37,6 +52,16 @@ public class KeyHandler implements KeyListener {
         if(code == KeyEvent.VK_E){
             interactPressed = true;
         }
+        //PAUSA
+        if (code == KeyEvent.VK_P) {
+            if (!pauseToggle) {
+                // Se la pausa non è in modalità toggle, imposta isPaused sulla pressione
+                pausePressed = true;
+            } else {
+                // Altrimenti, in modalità toggle, inverte lo stato di pausa quando si preme "P"
+                gamePanel.togglePause();
+            }
+        }
     }
 
     @Override
@@ -58,10 +83,24 @@ public class KeyHandler implements KeyListener {
         if(code == KeyEvent.VK_E){
             interactPressed = false;
         }
+        //PAUSA
+        if (code == KeyEvent.VK_P) {
+            if (!pauseToggle) {
+                // Se la pausa non è in modalità toggle, imposta isPaused sul rilascio
+                pausePressed = false;
+                if (gamePanel.isPaused()) {
+                    gamePanel.togglePause();
+                }
+            }
+        }
     }
 
     public boolean isShowDebugText() {
 
         return showDebugText;
+    }
+
+    public boolean isPausePressed() {
+        return pausePressed;
     }
 }
--- src/main/java/view/GamePanel.java
+++ src/main/java/view/GamePanel.java
@@ -38,7 +38,12 @@ public class GamePanel extends JPanel implements Runnable{
 
     int FPS = 60;
 
-    KeyHandler keyH = new KeyHandler();
+    private boolean isPaused = false;
+    private boolean pauseMenuVisible = false;
+
+
+    KeyHandler keyH = new KeyHandler(this);
+
     Thread gameThread;
     private Player player = new Player(this, keyH);
 
@@ -77,36 +82,42 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() {
 
-        double drawInterval = 1000000000.0/FPS;
+        double drawInterval = 1000000000.0 / FPS;
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
         long timer = 0;
         long drawCount = 0;
 
-        while(gameThread != null) {
-
+        while (gameThread != null) {
             currentTime = System.nanoTime();
-
             delta += (currentTime - lastTime) / drawInterval;
             timer += (currentTime - lastTime);
             lastTime = currentTime;
 
-            if(delta >= 1){
-                //1 UPDATE: update information character positions
-                update();
-                //2 DRAW: draw the screen with the updated information
-                paintImmediately(0,0, screenWidth,screenHeight);
+            if (delta >= 1) {
+                // 1 UPDATE: Aggiornamento delle informazioni sulla posizione del personaggio
+                if (!isPaused) {
+                    if (keyH.isPausePressed()) {
+                        togglePause();
+                    }
+                    update();
+                }
+
+                // 2 DRAW: Disegno dello schermo con le informazioni aggiornate
+                paintImmediately(0, 0, screenWidth, screenHeight);
                 delta--;
                 drawCount++;
             }
-            if(timer >= 1000000000){
+
+            if (timer >= 1000000000) {
                 System.out.println("FPS: " + drawCount);
                 drawCount = 0;
                 timer = 0;
             }
         }
     }
+
 
     public void update() {
         player.update();
@@ -134,29 +145,30 @@ public class GamePanel extends JPanel implements Runnable{
         super.paintComponent(g);
 
         this.graphics2D = (Graphics2D) g;
-
-        // Disegna sulla buffer
-        Graphics2D bufferGraphics = buffer.createGraphics();
-        // Cancella completamente l'immagine del buffer
-        bufferGraphics.clearRect(0, 0, screenWidth, screenHeight);
-        mapManager.draw(bufferGraphics);
-        for (Entity npc : npcList) {
-            npc.draw(bufferGraphics);
+        if(pauseMenuVisible){
+            drawPauseMenu(graphics2D);
         }
-        player.draw(bufferGraphics);
-        for (KeyItems items : keyItemsList) {
-            items.draw(bufferGraphics);
+        else {
+            // Disegna sulla buffer
+            Graphics2D bufferGraphics = buffer.createGraphics();
+            // Cancella completamente l'immagine del buffer
+            bufferGraphics.clearRect(0, 0, screenWidth, screenHeight);
+            mapManager.draw(bufferGraphics);
+            for (Entity npc : npcList) {
+                npc.draw(bufferGraphics);
+            }
+            player.draw(bufferGraphics);
+            for (KeyItems items : keyItemsList) {
+                items.draw(bufferGraphics);
+            }
+            ui.draw(bufferGraphics);
+            drawToTempScreen();
+            // Copia l'intera immagine buffer sulla schermata
+            g.drawImage(buffer, 0, 0, this);
+            graphics2D.dispose();
+            // Dispose del bufferGraphics
+            bufferGraphics.dispose();
         }
-        ui.draw(bufferGraphics);
-        drawToTempScreen();
-
-
-        // Copia l'intera immagine buffer sulla schermata
-        g.drawImage(buffer, 0, 0, this);
-
-        graphics2D.dispose();
-        // Dispose del bufferGraphics
-        bufferGraphics.dispose();
 
     }
 
@@ -199,6 +211,37 @@ public class GamePanel extends JPanel implements Runnable{
         graphics2D.drawString("Draw Time: " + passedTime, x, y);
     }
 
+    public void togglePause() {
+        if (isPaused) {
+            // Azioni da eseguire quando il gioco è in pausa
+            pauseMenuVisible = false;
+            // Altre azioni, ad esempio visualizzare un menu di pausa
+        } else {
+            // Azioni da eseguire quando il gioco riprende
+            pauseMenuVisible = true;
+            // Altre azioni, ad esempio chiudere il menu di pausa
+        }
+        isPaused = !isPaused;
+    }
+
+    private void drawPauseMenu(Graphics2D g2d) {
+        // Disegna il menu di pausa
+        // Ad esempio, puoi disegnare un rettangolo semi-trasparente con le opzioni del menu
+        g2d.setColor(new Color(0, 0, 0, 150)); // Colore nero semi-trasparente
+        g2d.fillRect(0, 0, screenWidth, screenHeight);
+
+        // Disegna le opzioni del menu di pausa
+        g2d.setColor(Color.WHITE);
+        g2d.setFont(new Font("Arial", Font.BOLD, 24));
+        String pauseText = "Paused";
+        int textWidth = g2d.getFontMetrics().stringWidth(pauseText);
+        int x = (screenWidth - textWidth) / 2;
+        int y = screenHeight / 2;
+        g2d.drawString(pauseText, x, y);
+
+        // Aggiungi altre opzioni del menu di pausa se necessario
+    }
+
     public Player getPlayer() {
         return this.player;
     }
@@ -235,6 +278,10 @@ public class GamePanel extends JPanel implements Runnable{
 
     public List<KeyItems> getKeyItemsList() {
         return keyItemsList;
+    }
+
+    public boolean isPaused() {
+        return isPaused;
     }
 
 
