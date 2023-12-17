b6ea7df97442cec8f96e81ad206514025877f5a9
Gio <gioacchino.augello02@gmail.com>
2023-12-17 20:49:31 +0100

    #Set title java window in Fata Draconis

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("2D Adventure");
+        window.setTitle("Fata Draconis");
 
         GamePanel gamePanel = new GamePanel();
 
